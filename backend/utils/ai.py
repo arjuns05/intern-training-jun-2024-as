761f4248a50f73d5216990c70b21e3b0c4846437
arjun sudhalkar <arjun_sudhalkar@colpal.com>
2024-06-10 14:20:17 -0400

    updating environment variable typo

--- backend/utils/ai.py
+++ backend/utils/ai.py
@@ -5,7 +5,7 @@ from utils.secret import get_secret
 
 secret = get_secret("secret/data/teams/intern-training/openai")
 OPENAI_API_KEY = secret["apikey"]
-base_url = os.getenv("OPENAI_BASE_URL", "https://api.dev.ai-hub.colpal.cloud/openai/v1")
+base_url = os.getenv("OPENAI_API_URL", "https://api.dev.ai-hub.colpal.cloud/openai/v1")
 
 
 client = OpenAI(api_key=OPENAI_API_KEY, base_url=base_url)
