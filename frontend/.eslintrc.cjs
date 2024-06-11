c2858d314f373b322574fc2dfe81e69449627d27
arjun sudhalkar <arjun_sudhalkar@colpal.com>
2024-06-11 10:34:57 -0400

    Install framework and edit indes.css file

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,21 +1,25 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  env: {
+    browser: true,
+    es2021: true,
   },
-}
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+    'plugin:react/jsx-runtime',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+    'react/jsx-no-bind': 'off',
+    'react/prop-types': 'off',
+    'jsx-a11y/label-has-associated-control': [2, {
+      controlComponents: ['Input'],
+      depth: 3,
+    }],
+  },
+};
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="guide" element={<Guide />} />
+        <Route path="configuration" element={<Configuration />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+function NavBar() {
+  return (
+    <div>
+      <hr />
+      <Link to="/">Home</Link>
+      {' | '}
+      <Link to="/guide">Prompting Guide</Link>
+      {' | '}
+      <Link to="/configuration">Configuration</Link>
+      <hr />
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      <NavBar />
+    </div>
+  );
+}
+
+function Guide() {
+  return (
+    <div>
+      <h2>Prompting Guide</h2>
+      <NavBar />
+    </div>
+  );
+}
+
+function Configuration() {
+  return (
+    <div>
+      <h2>Prompt Configuration</h2>
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
